c831444bdbc38f7010c1c082a47680a79d456a9b
artacone <artacone@gmail.com>
2021-09-08 21:05:16 +0700

    Add 1E

---
+++ 1/E.py
@@ -0,0 +1,25 @@
+def find_distance(p1, p2):
+    return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+
+
+def get_position(d, x, y):
+    if (0 <= x <= d) and (0 <= y <= d) and (y <= -x + d):
+        return 0
+    distances = [
+        (find_distance((x, y), (0, 0)), 1),
+        (find_distance((x, y), (d, 0)), 2),
+        (find_distance((x, y), (0, d)), 3)
+    ]
+    return min(distances)[1]
+
+
+assert get_position(5, 1, 1) == 0
+assert get_position(3, -1, -1) == 1
+assert get_position(4, 4, 4) == 2
+assert get_position(4, 2, 2) == 0
+
+
+if __name__ == '__main__':
+    d = int(input())
+    x, y = map(int, input().split())
+    print(get_position(d, x, y))
