7b22bb356da15358abc0d9b86d94524e8a2e6885
artacone <artacone@gmail.com>
2021-09-11 15:33:15 +0300

    Refactor

--- 2/E.py
+++ 2/E.py
@@ -1,11 +1,5 @@
 def get_min_time(folders):
-    total_diplomas = 0
-    min_folder = 0
-    for folder in folders:
-        total_diplomas += folder
-        if folder > min_folder:
-            min_folder = folder
-    return total_diplomas - min_folder
+    return sum(folders) - max(folders)
 
 
 assert get_min_time([2, 1]) == 1
