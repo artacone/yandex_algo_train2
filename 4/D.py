ad7978bb10cae1bbaa770c01aef55bae0d72e772
artacone <artacone@gmail.com>
2021-09-20 17:49:29 +0700

    Add 4D

--- 4/D.py
+++ 4/D.py
@@ -1,31 +1,31 @@
-def get_votes_by_party():
-    votes_by_party = {}
+def construct_parlament():
+    parties = []
+    order = 0
+    total_votes = 0
     while True:
         try:
             line = input()
         except EOFError:
             break
-        party, votes = line.split()
-        if party not in votes_by_party:
-            votes_by_party[party] = 0
-        votes_by_party[party] += int(votes)
-    return votes_by_party
-
-
-def construct_parlament():
-    votes_by_party = get_votes_by_party()
-    total_votes = sum(votes_by_party.values())
-    first_number, places_left = divmod(total_votes, 450)
-
-    places_by_party = []
-    for party in votes_by_party:
-        places_by_party.append([party, first_number])
-
-    while places_left > 0:
-
-        places_left -= 1
-
-
+        words = line.split()
+        votes = int(words[-1])
+        party = ' '.join(words[:-1])
+        parties.append([party, int(votes), order])
+        order += 1
+        total_votes += votes
+    first_number = total_votes / 450
+    places_left = 450
+    for party in parties:
+        places, priority = divmod(party[1], first_number)
+        party.append(places)
+        party.append(priority)
+        places_left -= places
+    parties.sort(key=lambda x: x[4], reverse=True)
+    for i in range(int(places_left)):
+        parties[i][3] += 1
+    parties.sort(key=lambda x: x[2])
+    for party in parties:
+        print(party[0], int(party[3]))
 
 
 if __name__ == '__main__':
