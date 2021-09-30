f9532eeeee7d19e1bd28005a2d86686ef9f0573a
artacone <artacone@gmail.com>
2021-10-01 00:36:58 +0700

    Add 5E (even worse TL)

--- 5/E.py
+++ 5/E.py
@@ -1,23 +1,33 @@
-def sum_of_three(a, b, c):
-    c_num_to_index = {}
-    for i in range(len(c)):
-        if c[i] not in c_num_to_index:
-            c_num_to_index[c[i]] = i
-    for i in range(1, len(a)):
-        for j in range(1, len(b)):
-            ck = s - (a[i] + b[j])
-            if ck in c_num_to_index:
-                return i-1, j-1, c_num_to_index[ck]
-    return -1
+# В надежде, что прошлое решение TLилось из-за большой константы хэштаблиц,
+# переписал код как в разборе. Стало работать еще медленнее.
+# Так что я просто сдал тот же алгоритм, но написанный на Go
+
+def get_enumed_arr():
+    array = list(map(int, input().split()))[1:]
+    for i in range(len(array)):
+        array[i] = array[i], i
+    return array
 
 
 if __name__ == '__main__':
     s = int(input())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    c = list(map(int, input().split()))
-    ans = sum_of_three(a, b, c[1:])
-    if ans == -1:
-        print(ans)
-    else:
+    a = get_enumed_arr()
+    b = get_enumed_arr()
+    c = get_enumed_arr()
+    a.sort()
+    b.sort()
+    c.sort(key=lambda x: (x[0], -x[1]))
+    is_first = True
+    sum_of_three = 0
+    for a_val, a_pos in a:
+        c_iter = len(c) - 1
+        for b_val, b_pos in b:
+            while c_iter > 0 and a_val + b_val + c[c_iter][0] > s:
+                c_iter -= 1
+            if a_val + b_val + c[c_iter][0] == s and (is_first or (a_pos, b_pos, c_iter) < ans):
+                ans = a_pos, b_pos, c[c_iter][1]
+                is_first = False
+    if not is_first:
         print(*ans)
+    else:
+        print(-1)
